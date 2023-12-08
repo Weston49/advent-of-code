16cd0f02602adb4b402905c40d40ab89958d96b6
Weston49 <weston.agreda@gmail.com>
2023-12-08 13:16:42 -0500

    Some more days done ez clap

--- 2023/day6/main.cpp
+++ 2023/day6/main.cpp
@@ -3,11 +3,42 @@
 #include <cstdio>
 #include <algorithm>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 
 int main(){
+  string s;
+  long tmp, count, dist, total;
+  long i, j;
+  string times;
+  string dists;
   
+  getline(cin, s);
+  s = s.substr(9);
+  stringstream ss(s);
+  while(ss >> tmp){
+    times += to_string(tmp);
+  }
+
+  getline(cin, s);
+  s = s.substr(9);
+  stringstream ss2(s);
+  while(ss2 >> tmp){
+    dists += to_string(tmp);
+  }
+
+  cout << times << endl;
+  cout << dists << endl;
+  count = 0;
+  total = 1;
+  for(j = 0; j < stol(times); j++){
+    dist = j * (stol(times) - j);
+    // cout << dist << endl;
+    if(dist > stol(dists)) count++;
+  }
+
+  cout << count << endl;
 
   return 0;
 }
