abcadafc339a334f49457d0a13db1fe17c40f675
Weston49 <weston.agreda@gmail.com>
2024-10-21 12:42:21 -0400

    2019 day1

--- 2019/day1/main.cpp
+++ 2019/day1/main.cpp
@@ -7,7 +7,14 @@
 using namespace std;
 
 int main(){
-  
-
+  int total = 0;
+  int n;
+  while(cin >> n){
+    while(n > 0){
+      total += max(0, (n/3) - 2);
+      n = (n/3) - 2;
+    }
+  }
+  cout << total << endl;
   return 0;
 }
