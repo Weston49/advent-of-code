849aa203936b8b434c811a09419d790d038ef9e9
Weston49 <weston.agreda@gmail.com>
2024-12-03 13:31:47 -0500

    making day2 as pythonic and unreadable as possible

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -4,45 +4,11 @@ def read_input(file_path):
     return file.readlines()
 d = read_input('./input.txt')
 
-#Room for helper functions
-def is_row_safe(row):
-  safe = True
-  inc = int(row[0]) < int(row[1])
-  for i in range(len(row)-1):
-    if(int(row[i]) == int(row[i+1])):
-      return False
-    if abs(int(row[i]) - int(row[i+1])) > 3:
-      return False
-    if inc and int(row[i]) > int(row[i+1]):
-      return False
-    if not inc and int(row[i]) < int(row[i+1]):
-      return False
-  return True
+d = [d[i].split() for i in range(len(d))]
 
-#Room for cleaning up input
-levels = []
-for i in range(len(d)):
-  levels.append(d[i].split())
+def is_report_safe(row):  
+  return all((abs(int(row[i]) - int(row[i+1])) < 4) for i in range(len(row)-1)) and (all(int(row[i]) > int(row[i+1]) for i in range(len(row)-1)) or all(int(row[i]) < int(row[i+1]) for i in range(len(row)-1)))
 
-#Part 1
-total = 0
+print("Part 1: ", sum(1 for report in d if is_report_safe(report)))
 
-for row in levels:
-  if is_row_safe(row):
-    total += 1
-    
-print("Part 1: ", total)
-
-#Part 2
-total = 0
-
-for row in levels:
-  for i in range(len(row)):
-    new_row = row[:i] + row[i+1:]
-    if is_row_safe(new_row):
-      total += 1
-      break
-
-
-
-print("Part 2: ", total)
+print("Part 2: ", sum(1 for report in d if(any(is_report_safe(report[:i] + report[i+1:]) for i in range(len(report))))))
